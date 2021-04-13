e72f404b72ee207f842db10b98c99e8dfddccdab
Marcos <marcoscianziogamero@gmail.com>
2021-04-13 10:46:20 -0300

    adding files

---
+++ docs/precache-manifest.62e83fb9108c03db844c368e8957c828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bfb1e66bd5253305ec525d76a61283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68399a17f31d31ddd16",
+    "url": "/static/js/2.213c4715.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.213c4715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a4faac6d64d960797a",
+    "url": "/static/js/main.92e35ea1.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
